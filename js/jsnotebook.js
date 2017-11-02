ebee796887f82ba08073055b284ccb66ab78f8ea
Hiroshi Suda <suda@net.it-chiba.ac.jp>
2017-11-02 18:44:05 +0900

    onsole.logのバグ修正

--- js/jsnotebook.js
+++ js/jsnotebook.js
@@ -1,4 +1,13 @@
 
+var jnsPrint = ( element, string ) => {
+    element.innerHTML += string + "<br>";
+    console.log( string );
+}
+
+var jnsError = ( element, string ) => {
+    element.innerHTML += "<span class='err'>" + string + "</span><br>";
+}
+
 // Run（実行）ボタンへのイベントを付加するメソッド
 var setRun2 = ( div, element ) => {
     div.addEventListener('click', function() {
@@ -8,36 +17,51 @@ var setRun2 = ( div, element ) => {
         var script = document.createElement('script');
         script.innerHTML = program
             .replace( /<br>/g, "\r\n" )
-            .replace( /&nbsp;/,"");
+            .replace( /&nbsp;/,"")
+            .replace( /console.log\(/g, "jnsPrint( output," );
         document.body.appendChild( script );
     });
 };
 
-// Run（実行）ボタンへのイベントを付加するメソッド
-var setRun = ( div, element ) => {
+// Run（実行）ボタンへのイベントを付加するメソッドのeval版
+// 枠をまたいだ実行に対応していないのでボツ
+var setRun = function( div, element )  {
     div.addEventListener('click', function() {
         output = element.parentNode.lastElementChild;
         var program = element.innerHTML;
 //console.log( program ); // デバッグ用
+        var dummyEval = eval;
         var script = program
             .replace( /<br>/g, "\r\n" )
-            .replace( /&nbsp;/,"");;
-        eval( script );
+            .replace( /&nbsp;/,"")
+            .replace( /console.log\(/g, "jnsPrint( output," );
+        try {
+            dummyEval( script );
+        } catch (e) {
+            jnsError( output, e );
+            throw( e );
+        }
     });
 };
 
 // Once（一度だけ実行）ボタンへのイベントを付加するメソッド
 var setOnce = ( div, element ) => {
     div.addEventListener('click', function() {
-        output = element.parentNode.lastElementChild;
+        var output = element.parentNode.lastElementChild;
         var program = element.innerHTML;
 //console.log( program ); // デバッグ用
-        var script = document.createElement('script');
-        script.innerHTML = program
+        var dummyEval = eval;
+        //var script = document.createElement('script');
+        script = program
             .replace( /<br>/g, "\r\n" )
-            .replace( /&nbsp;/,"");;
-        //eval( script );
-        document.body.appendChild( script );
+            .replace( /&nbsp;/,"")
+            .replace( /console.log\(/g, "jnsPrint( output," );
+        try {
+            eval( script );
+        } catch (e) {
+            jnsError( output, e );
+            throw( e );
+        }
         div.disabled = true;
     });
 };
@@ -45,27 +69,27 @@ var setOnce = ( div, element ) => {
 // Sandbox（実行）ボタンへのイベントを付加するメソッド
 var setSandbox = ( div, element ) => {
     div.addEventListener('click', function() {
-        output = element.parentNode.lastElementChild;
+        var output = element.parentNode.lastElementChild;
         var program = element.innerHTML;
 //console.log( program ); // デバッグ用
         var script = "(function(){" + program
-            .replace( /<br>/g, "\r\n" )
-            .replace( /&nbsp;/,"")
+                .replace( /<br>/g, "\r\n" )
+                .replace( /&nbsp;/,"")
             + "})()";;
-console.log( script );
+        console.log( script );
         eval( script );
     });
 };
 
 window.addEventListener('load', function() {
     showdown.setFlavor('github');
-    
+
     output = undefined;
-    console.log = ( value ) =>  {
-        if( output != undefined ) {
-            output.innerHTML += value + "<br>";
-        }
-    }
+//     console.log = ( value ) =>  {
+//         if( output != undefined ) {
+//             output.innerHTML += value + "<br>";
+//         }
+//     }
 
     // mdタグを拾ってMarkdownへの変換処理をおこなう
     var docs = document.querySelectorAll( 'jsn-md' );
@@ -74,20 +98,20 @@ window.addEventListener('load', function() {
         value.innerHTML = conv.makeHtml( value.innerHTML );
         var div = document.createElement('button');
     });
-    
+
     // &, >, <を置き換える
     var pres = document.querySelectorAll( 'pre code.javascript' );
     pres.forEach( function( value ) {
         let code = value.innerHTML.replace( /&amp;/g, '&' ).replace( /&lt;/g, '<' ).replace( /&gt;/g, '>' );
         value.innerHTML = code;
     });
-    
+
     // runnableクラスの付いた要素に実行ボタンを付ける
     var pres = document.querySelectorAll( '.runnable' );
     pres.forEach( function( value ) {
         var btn = document.createElement('button');
         btn.textContent = "Run";
-        setRun2( btn, value );
+        setRun( btn, value );
         btn.contentEditable = false;
         let parent = value.parentNode;
         parent.appendChild( document.createElement('p') );
@@ -96,8 +120,8 @@ window.addEventListener('load', function() {
         parent.style.background = "#f8fcfc";
         parent.style.border = "solid 1px #80a0a0";
     });
-    
-     // onceクラスの付いた要素に実行ボタンを付ける
+
+    // onceクラスの付いた要素に実行ボタンを付ける
     var pres = document.querySelectorAll( '.once' );
     pres.forEach( function( value ) {
         var btn = document.createElement('button');
@@ -112,7 +136,7 @@ window.addEventListener('load', function() {
         parent.style.border = "solid 1px #80a0a0";
     });
 
-     // onceクラスの付いた要素に実行ボタンを付ける
+    // onceクラスの付いた要素に実行ボタンを付ける
     var pres = document.querySelectorAll( '.sandbox' );
     pres.forEach( function( value ) {
         var btn = document.createElement('button');
@@ -126,7 +150,7 @@ window.addEventListener('load', function() {
         parent.style.background = "#f8fcfc";
         parent.style.border = "solid 1px #80a0a0";
     });
-    
+
     // editableクラスの付いた要素を編集可能にする
     var edit = document.querySelectorAll( '.editable' );
     edit.forEach( function( value ) {
@@ -137,7 +161,7 @@ window.addEventListener('load', function() {
     // hlクラスの付いた要素にSyntaxHighliteを行う
     var code = document.querySelectorAll( 'pre code.hl, pre code.html' );
     code.forEach( function( block ) {
-        block.innerHTML = block.innerHTML.replace( /&amp;/g, '&' ); 
+        block.innerHTML = block.innerHTML.replace( /&amp;/g, '&' );
         hljs.highlightBlock( block );
     });
 });
