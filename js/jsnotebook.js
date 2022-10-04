285a0472c7791c50ccb39fd13d1d3969986a587e
Hiroshi Suda <suda@SudanoMacBook-Air.local>
2022-10-04 19:06:55 +0900

    jsnotebook.jsを刷新

--- js/jsnotebook.js
+++ js/jsnotebook.js
@@ -1,146 +1,176 @@
+make_button = ( btn_text, element, console_number, check_console, settlement ) => {
+    let btn = document.createElement('button');
+    btn.textContent = btn_text;
+    let parent = element.parentNode;
+    // ボタンの位置調整のためだけのp要素
+    parent.appendChild( document.createElement('p') );
+    parent.appendChild( btn );
 
-var jnsPrint = ( element, string ) => {
+    btn.addEventListener('click', () => {
+        let program = element.innerHTML;
+
+        program = program
+        .replace( /&nbsp;/g," ")
+        .replace( /&lt;/g, "<" )
+        .replace( /&gt;/g, ">")
+        .replace( /<br>/g, "\r\n" );
+        if( check_console ) {
+            program = program.replace( /console.log\(/g, "jsnPrint( output[" + console_number + "]," );
+        }
+        // evalをそのまま使用すると，別の枠に変数の内容を受け渡せないため，ダミーを使用する
+        let dummyEval = eval;
+        let errOutput = output[ console_number ];
+
+        try {
+            dummyEval( program );
+        } catch (e) {
+            jsnError( errOutput, e );
+            throw( e );
+        }
+        btn.disabled = settlement;
+    });
+}
+
+class Executable {
+    constructor( element ) {
+        this.element = element;
+        output ||= [];
+        output_number ||= 0;
+        this.console_number = false;
+    }
+
+    check_console() {
+        return ( this.element.className.indexOf('console') != -1 );
+    }
+
+    add_console() {
+        this.console_number = output_number;
+        let parent = this.element.parentNode;
+        let con = document.createElement( 'pre' );
+        con.classList.add( 'console' );
+        parent.appendChild( con );
+        output.push( con );
+        output_number += 1;
+    }
+}
+class Runnable extends Executable{
+    add_button() {
+        // let btn = document.createElement('button');
+        // btn.textContent = "Run";
+        // let parent = this.element.parentNode;
+        // parent.appendChild( document.createElement('p') );
+        // parent.appendChild( btn );
+        //parent.appendChild( document.createElement('p').appendChild(btn) )
+
+        make_button( "Run", this.element, this.console_number, this.check_console(), false );
+
+    }
+}
+class Once extends Executable{
+    add_button(){
+        // let btn = document.createElement('button');
+        // btn.textContent = "Once";
+        // let parent = this.element.parentNode;
+        // parent.appendChild( document.createElement('p') );
+        // parent.appendChild( btn );
+        //parent.appendChild( document.createElement('p').appendChild(btn) )
+
+        make_button( "Once", this.element, this.console_number, this.check_console(), true );
+    }
+}
+class Sandbox extends Executable {
+    add_button(){
+        let btn = document.createElement('button');
+        btn.textContent = "Run";
+        let parent = this.element.parentNode;
+        parent.appendChild( document.createElement('p') );
+        parent.appendChild( btn );
+
+        btn.addEventListener('click', () => {
+            let program = this.element.innerHTML;
+            let script = program;
+            if( this.check_console ) {
+                script = program.replace( /console.log\(/g, "jsnPrint( output[" + this.console_number + "]," );
+            }
+            try {
+                eval( script );
+            } catch (e) {
+                jsnError( output, e );
+                throw( e );
+            }
+        });
+    }
+}
+class JSNotebook {
+    constructor( markdown, conv ) {
+        this.markdown = markdown;
+        this.conv = conv;
+        this.executable = [];
+    }
+    md2html() {
+        this.markdown.innerHTML = this.conv.makeHtml( this.markdown.innerHTML );
+    }
+    setup_executable() {
+        //console.log(this.markdown);
+        let pre_tags = this.markdown.querySelectorAll('pre code.javascript');
+        //console.log(pre_tags);
+        pre_tags.forEach( elm => {
+            elm.textContent = elm.textContent
+            .replace( /&nbsp;/g," ")
+            .replace( /&lt;/g, "<" )
+            .replace( /&gt;/g, ">")
+            .replace( /<br>/g, "\r\n" );
+            if( elm.className.indexOf( 'runnable') != -1 )   this.executable.push( new Runnable( elm ) );
+            else if( elm.className.indexOf( 'once') != -1 )   this.executable.push( new Once( elm ) );
+            else if( elm.className.indexOf( 'sandbox') != -1 )   this.executable.push( new Sandbox( elm ) );
+         });
+        //console.log(this.executable);
+
+        this.executable.forEach( elm => {
+            //console.log( elm.check_console() );
+            if( elm.check_console() )   elm.add_console();
+            elm.add_button();
+            //console.log( elm );
+        })
+    }
+}
+const jsnPrint = ( element, string ) => {
 	if( element.className.indexOf( 'console' ) != -1 ) {
 		 element.innerHTML += string + "<br>";
 	}
-	console.log( string );
 }
 
-var jnsError = ( element, string ) => {
+const jsnError = ( element, string ) => {
     if( element.className.indexOf( 'console' ) != -1 ) {
     	element.innerHTML += "<span class='err'>" + string + "</span><br>";
     }
 }
 
-// Run（実行）ボタンへのイベントを付加するメソッド
-var setRun2 = ( div, element ) => {
-    div.addEventListener('click', function() {
-        output = element.parentNode.lastElementChild;
-        var program = element.innerHTML;
-//console.log( program ); // デバッグ用
-        var script = document.createElement('script');
-        script.innerHTML = program
-            .replace( /<br>/g, "\r\n" )
-            .replace( /&nbsp;/," ")
-            .replace( /console.log\(/g, "jnsPrint( output," );
-        document.body.appendChild( script );
-    });
-};
-
-// Run（実行）ボタンへのイベントを付加するメソッドのeval版
-// 枠をまたいだ実行に対応していないのでボツ
-// 強引に枠を跨いで実行できるように改変
-var setRun = function( div, element )  {
-    div.addEventListener('click', function() {
-        output = element.parentNode.lastElementChild;
-        var program = element.innerHTML;
-//console.log( program ); // デバッグ用
-        var dummyEval = eval;
-        var script = program
-            .replace( /<br>/g, "\r\n" )
-            .replace( /&nbsp;/g," ").replace( /&amp;/g, '&' ).replace( /&lt;/g, '<' ).replace( /&gt;/g, '>'  )
-            .replace( /console.log\(/g, "jnsPrint( output," );
-/*        if( output.className.indexOf( 'console' ) != -1 ) {
-            script = script.replace( /console.log\(/g, "jnsPrint( output," );
-        }*/
-        //console.debug( script );
-        try {
-            dummyEval( script );
-        } catch (e) {
-            jnsError( output, e );
-            throw( e );
-        }
-    });
-};
-
-// Once（一度だけ実行）ボタンへのイベントを付加するメソッド
-var setOnce = ( div, element ) => {
-    div.addEventListener('click', function() {
-        output = element.parentNode.lastElementChild;
-//console.log( output );
-        var program = element.innerHTML;
-//console.log( program ); // デバッグ用
-        var dummyEval = eval;
-        //var script = document.createElement('script');
-        var script = program
-            .replace( /<br>/g, "\r\n" )
-            .replace( /&nbsp;/g," ")
-            .replace( /console.log\(/g, "jnsPrint( output," );
- //console.log( output );
-        try {
-            dummyEval( script );
-        } catch (e) {
-            jnsError( output, e );
-            throw( e );
-        }
-        div.disabled = true;
-    });
-};
-
-// Sandbox（実行）ボタンへのイベントを付加するメソッド
-var setSandbox = ( div, element ) => {
-    div.addEventListener('click', function() {
-      output = element.parentNode.lastElementChild;
-        var program = element.innerHTML;
-//console.log( program ); // デバッグ用
-        var dummyEval = eval;
-        var script = "(function(){" + program
-                .replace( /<br>/g, "\r\n" )
-                .replace( /&nbsp;/g," ")
-                .replace( /console.log\(/g, "jnsPrint( output," )
-            + "})()";;
-        try {
-            dummyEval( script );
-        } catch (e) {
-            jnsError( output, e );
-            throw( e );
-        }
-    });
-};
-
 window.addEventListener('DOMContentLoaded', function() {
     showdown.setFlavor('github');
 
-    output = undefined;
-//     console.log = ( value ) =>  {
-//         if( output != undefined ) {
-//             output.innerHTML += value + "<br>";
-//         }
-//     }
+    // consoleに相当するオブジェクトを入れておく配列
+    // evalするときにグローバル変数でないとエラーになるので，
+    // 回避するためにゴチャゴチャしている
+    output = [];
+    output_number = 0;
 
-    // mdタグを拾ってMarkdownへの変換処理をおこなう
+    // jsn-mdタグを拾ってMarkdownへの変換処理をおこなう
     var docs = document.querySelectorAll( 'jsn-md' );
     var conv = new showdown.Converter(  );
-    docs.forEach( function( value ) {
-        value.innerHTML = conv.makeHtml( value.innerHTML );
-        var div = document.createElement('button');
+    docs.forEach( function( markdown ) {
+        let jsn = new JSNotebook( markdown, conv );
+        jsn.md2html();
+        jsn.setup_executable();
     });
 
-    // &, >, <を置き換える
-    var pres = document.querySelectorAll( 'pre code.javascript' );
-//    console.log(pres);
-    pres.forEach( function( value ) {
-//        console.log(value); // デバッグ用
-        let code = value.textContent.replace( /&amp;/g, '&' ).replace( /&lt;/g, '<' ).replace( /&gt;/g, '>' ).replace( /&nbsp;/g, ' ' );
-        value.innerHTML = code;
-    });
+
 
     // runnableクラスの付いた要素に実行ボタンを付ける
     var pres = document.querySelectorAll( '.runnable' );
     pres.forEach( function( value ) {
-        var btn = document.createElement('button');
-        btn.textContent = "Run";
-        setRun( btn, value );
-        btn.contentEditable = false;
+        //console.log( value );
         let parent = value.parentNode;
-        parent.appendChild( document.createElement('p') );
-        parent.appendChild( btn );
-        //console.log( value.className );
-        if( value.className.indexOf( 'console' ) != -1 ) {
-        	con = document.createElement( 'pre' );
-        	con.classList.add( 'console' );
-        	parent.appendChild( con );
-        }
         parent.style.background = "#f8fcfc";
         parent.style.border = "solid 1px #80a0a0";
     });
@@ -148,18 +178,7 @@ window.addEventListener('DOMContentLoaded', function() {
     // onceクラスの付いた要素に実行ボタンを付ける
     var pres = document.querySelectorAll( '.once' );
     pres.forEach( function( value ) {
-        var btn = document.createElement('button');
-        btn.textContent = "Once";
-        setOnce( btn, value );
-        btn.contentEditable = false;
         let parent = value.parentNode;
-        parent.appendChild( document.createElement('p') );
-        parent.appendChild( btn );
-        if( value.className.indexOf( 'console' ) != -1 ) {
-        	con = document.createElement( 'pre' );
-        	con.classList.add( 'console' );
-        	parent.appendChild( con );
-        }
         parent.style.background = "#f8fcfc";
         parent.style.border = "solid 1px #80a0a0";
     });
@@ -167,18 +186,7 @@ window.addEventListener('DOMContentLoaded', function() {
     // sandboxクラスの付いた要素に実行ボタンを付ける
     var pres = document.querySelectorAll( '.sandbox' );
     pres.forEach( function( value ) {
-        var btn = document.createElement('button');
-        btn.textContent = "Sandbox";
-        setSandbox( btn, value );
-        btn.contentEditable = false;
         let parent = value.parentNode;
-        parent.appendChild( document.createElement('p') );
-        parent.appendChild( btn );
-        if( value.className.indexOf( 'console' ) != -1 ) {
-        	con = document.createElement( 'pre' );
-        	con.classList.add( 'console' );
-        	parent.appendChild( con );
-        }
         parent.style.background = "#f8fcfc";
         parent.style.border = "solid 1px #80a0a0";
     });
